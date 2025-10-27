e90ccbc004240d8f023733457d5b42c51ae98b74
Divya <divya.nyadav6351@gmail.com>
2025-10-27 14:55:17 +0530

    Flask

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -17,7 +16,8 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            # use dill for robust serialization of Python objects (supports more types than pickle)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -56,7 +56,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            # use dill to load objects saved with dill
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
